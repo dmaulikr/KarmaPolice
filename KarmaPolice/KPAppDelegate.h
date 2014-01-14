76aad2aa8b410d6469689ec60e1ff8992946b95d
Elizabeth Lambos <elizabeth.lambos@gmail.com>
2014-01-14 09:23:37 -0500

    Practice with a comment in the KPAppDelegate.h file

--- KarmaPolice/KPAppDelegate.h
+++ KarmaPolice/KPAppDelegate.h
@@ -5,6 +5,7 @@
 //  Created by Gil Shulman on 1/13/14.
 //  Copyright (c) 2014 Karma Police. All rights reserved.
 // Gil and Liz's code!
+// Liz and Gil Comitting practice
 //
 
 #import <UIKit/UIKit.h>
