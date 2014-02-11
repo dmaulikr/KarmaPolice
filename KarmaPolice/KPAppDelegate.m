0c30db198b04a8c4bf480eb5b5b6eea62bf98c55
Gil Shulman <gil.shulman@gmail.com>
2014-02-11 16:12:46 +0200

    Saving User Data Works

--- KarmaPolice/KPAppDelegate.m
+++ KarmaPolice/KPAppDelegate.m
@@ -91,26 +91,26 @@ view controllers: (1) KPGetKarmaViewController (2) new query (3) activity (4) in
         NSDictionary *userData = (NSDictionary *)result;
         
         NSString *facebookID = userData[@"id"];
-        NSString *name = userData[@"name"];
+        NSString *username = userData[@"name"];
         NSString *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
-        //NSString *AllFBData =[NSString stringWithFormat:@"%@ %@ %@ %@", facebookID,name,location,pictureURL];
+
         
-        PFQuery *userQuery = [PFQuery queryWithClassName:@"User"];
+        PFQuery *query = [PFQuery queryWithClassName:@"_User"];
+        
         // Retrieve the object by id
-        PFObject *userObject = [PFUser currentUser];
-    
-        whereKey:@"user" equalTo:[PFObject objectWithoutDataWithClassName:@"User" objectId:[PFUser currentUser].objectId]];
-        
-        
-        [userQuery getObjectInBackgroundWithId:userObject.objectId block:^(PFObject *userRecord, NSError *error) {
-            userRecord[@"UserImageURL"] = pictureURL;
-            userRecord[@"strUserName"] = name;
-            //user[@"cheatMode"] = @NO;
-            [userRecord saveInBackground];
+        [query getObjectInBackgroundWithId:[PFUser currentUser].objectId block:^(PFObject *user, NSError *error) {
+            
+            // Now let's update it with some new data. In this case, only cheatMode and score
+            // will get sent to the cloud. playerName hasn't changed.
+            user[@"UserImageURL"] = [NSString stringWithFormat:@"%@", pictureURL];
+            user[@"strUserName"] = username;
+            [user saveInBackground];
+            
         }];
-        NSLog(@"Problem is:", error);
+        
     }];
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
