f3e676ee1cb4e90c581574fb6205bc1fa3692cc1
Gil Shulman <gil.shulman@gmail.com>
2014-03-28 16:09:04 -0400

    Table View

--- KPMyKarmaViewController.m
+++ KPMyKarmaViewController.m
@@ -35,11 +35,11 @@
         cell = [nib objectAtIndex:0];
     }
     
-    cell.timeLabel.text = @"Oy";
-    cell.questionLabel.text = @"Oy";
+    cell.timeLabel.text = [questionsArray[indexPath.row] objectForKey:@"createdAt"];
+    cell.questionLabel.text = [questionsArray[indexPath.row] objectForKey:@"questionText"];
     cell.resultsLabel.text = @"Oy";
-    cell.peopleLabel.text = @"Oy";
-    cell.privacySegments.selectedSegmentIndex = 1;
+    cell.peopleLabel.text = @"Oy X2";
+    cell.privacySegments.selectedSegmentIndex = [questionsArray[indexPath.row] objectForKey:@"privacyLevel"];
     
     
     /*
@@ -69,7 +69,10 @@
     _QuestionTableView.dataSource = self;
     
     // Do any additional setup after loading the view.
-    questionsArray = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    // questionsArray = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    [self populateArray];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -77,6 +80,40 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) populateArray {
+    PFQuery *query = [PFQuery queryWithClassName:@"TblQuestions"];
+    PFUser *user = [PFUser currentUser];
+    [query whereKey:@"UserId" equalTo:user.objectId];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error && objects.count > 0) {
+            // The find succeeded
+            NSLog(@"Successfully retrieved %d questions", objects.count);
+            int questionIndex;
+            questionIndex = 0;
+            while (objects.count > questionIndex){
+                PFObject *object = objects[questionIndex];
+                
+                // Get the date from parse and format it:
+                NSDate *createdAt = [object createdAt];
+                NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+                [dateFormat setDateFormat:@"EEE, MMM d, h:mm a"];
+                NSString* strCreatedAt = [NSString stringWithFormat:@"Lasted Updated: %@", [dateFormat stringFromDate:createdAt]];
+         
+                NSArray *keys = [NSArray arrayWithObjects:@"questionText", @"CreatedAt", @"privacyLevel",@"percantageYes",@"totalNumberOfAnswers", nil];
+                NSArray *arrObjects = [NSArray arrayWithObjects:[object objectForKey:@"Question"],strCreatedAt,[object objectForKey:@"allKP"],@"0",@"0", nil];
+                NSDictionary *dictionary = [NSDictionary dictionaryWithObjects:arrObjects
+                                                                       forKeys:keys];
+                
+                NSMutableDictionary *mutableDict = [dictionary mutableCopy];
+                [questionsArray[questionIndex] addObject:mutableDict];
+                questionIndex++;
+            }
+        }
+    }];
+}
+
 
 /*
 #pragma mark - Navigation
